36bebebd0faebe5210ca339dd62252271289fca6
nurbaki <nurbakibayansal11@gmail.com>
2022-09-09 09:39:12 +0200

    09 09 2022 new lesson

--- 009_CRUD/fscohort/models.py
+++ 009_CRUD/fscohort/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 class Student(models.Model):
+    GENDER_MALE = 0
+    GENDER_FEMALE = 1
+    GENDER_CHOICES = [(GENDER_MALE, 'Male'), (GENDER_FEMALE, 'Female')]
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     number = models.IntegerField(blank=True, null=True)
+    email = models.EmailField(unique=True)
+    path = models.TextField(max_length=50)
+    gender = models.IntegerField(choices=GENDER_CHOICES)
+    register_date = models.DateField(auto_now_add=True)
+    last_update_date = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
 
     class Meta:
         ordering = ("first_name",)
